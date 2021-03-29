c2c6f7a2eebb9e3df2f5aa1bd370955b8304ffe9
robinhermosa <79049505+robinhermosa@users.noreply.github.com>
2021-03-29 13:28:03 +0800

    Update settings.py

--- rgpi_mcc_dashboard/settings.py
+++ rgpi_mcc_dashboard/settings.py
@@ -39,7 +39,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
