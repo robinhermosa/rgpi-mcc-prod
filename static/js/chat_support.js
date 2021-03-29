ebfe5deb457528035b4173d615da1687229d56ae
robinhermosa <79049505+robinhermosa@users.noreply.github.com>
2021-03-29 11:07:27 +0800

    Add files via upload

---
+++ static/js/chat_support.js
@@ -0,0 +1,9 @@
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/6014be91a9a34e36b971f19f/1et8ggr14';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+})();
